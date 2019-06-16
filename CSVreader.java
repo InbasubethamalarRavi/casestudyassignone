881d5601ac521618adeadeeade9c4a2c0a518e85
InbasubethamalarRavi <51881045+InbasubethamalarRavi@users.noreply.github.com>
2019-06-16 21:59:42 +0530

    Add files via upload

---
+++ CSVreader.java
@@ -0,0 +1,122 @@
+package com;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class CSVreader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//List<Employee> files=readFileFromCsv("input.csv");
+		String z="C:\\Users\\User\\eclipse-workspace\\csv1\\inputfile.csv";
+		List<Employee1> files = CSVreader.readCSVfile(z);
+		//for(Employee1 f: files)
+		//
+			//System.out.println(f.getFirst_name());
+		//}
+		
+		List<Employee> A = Sortedlist(files);
+		for(Employee ab: A)
+		{
+			System.out.println(ab.getFirst_name());
+		}
+		//filterFileBasedOnCity(files);
+		for(Employee1 b: files)
+		{
+			if(b.getCity().equals("New York"))
+			{
+				System.out.println(b.getFirst_name()+" "+b.getLast_name()+" "+b.getCompany_name());
+			}
+		}
+	}
+	public static List<Employee> Sortedlist(List<Employee1> l)
+	{
+		
+		for(int i=0;i<l.size()-1;i++)
+		{
+			for(int j=0;j<l.size()-1;j++)
+			{
+				if(l.get(i).getFirst_name().compareTo(l.get(j).getFirst_name())<0)
+				{
+					Collections.swap(l,i,j);
+				}
+					
+			}
+		}
+		return l;
+	
+	}
+public CSVreader()
+{
+	super();
+}
+	private static List<Employee> readCSVfile(String fileName)
+	{
+		List<Employee> files=new ArrayList<>();
+		Path PathtofFile=Paths.get(fileName);//to get the filepath
+		try
+		//(BufferedReader br=Files.newBufferedReader(Pathtofile,StandardCharsets.US_ASCII))
+		{ 
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String line=br.readLine();
+			while(line!=null)
+			{
+				String[] attributes = line.split(",");
+				Employee file = createList(attributes);
+				files.add(file);
+				line=br.readLine();
+			}
+		} catch(IOException ioe)
+		{
+			ioe.printStackTrace(); //it prints the error class and error line
+		}
+		return files;
+	}
+	
+	/*private static Employee create_files(String[] attributes) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
+	private static Employee createList(String[] metadata)
+	{
+		String first_name=metadata[0];
+		String last_name=metadata[1];
+		String company_name=metadata[2];
+		String address=metadata[3];
+		String city=metadata[4];
+		String country=metadata[5];
+		String state=metadata[6];
+		String zip=metadata[7];
+		String phone_num1=metadata[8];
+		String phone_num2=metadata[9];
+		String e_mail=metadata[10];
+		String web=metadata[11];
+		return new Employee(first_name.replace("\"", ""),last_name.replace("\"", ""),company_name.replace("\"", ""),address.replace("\"", ""),city.replace("\"", ""),country.replace("\"", ""),state.replace("\"", ""),zip.replace("\"", ""),phone_num1.replace("\"", ""),phone_num2.replace("\"", ""),e_mail.replace("\"", ""),web.replace("\"", ""));
+	
+	}
+	
+	/*public static void filterFileBasedOnCity(List<Employee> m)
+	{
+		//List<Employee> ans=m.stream().filter(i->i.getcity().equals("New York")).collect(Collectors.toList());
+		for(Employee f: m)
+		{
+			if(f.getcity().equals("\"New York\""))
+			{
+				System.out.println(f.getFirst_name()+" "+f.getlastName()+" "+f.getcity());
+			}
+			
+		}
+		
+	}*/
+
+	}
+
---
+++ Employee.java
@@ -0,0 +1,117 @@
+package com;
+	
+	public class Employee {
+		private String first_name;
+		private String last_name;
+		private String company_name;
+		private String address;
+		private String city;
+		private String country;
+		private String state;
+		private String zip;
+		private String phone_num1;
+		private String phone_num2;
+		
+		private String e_mail;
+		private String web_mail;
+		public Employee1(String first_name, String last_name, String company_name,
+				String address, String city, String country, String state,
+				String zip, String phone_num1, String phone_num2, String e_mail,
+				String web_mail) {
+			super();
+			this.first_name = first_name;
+			this.last_name = last_name;
+			this.company_name = company_name;
+			this.address = address;
+			this.city = city;
+			this.country = country;
+			this.state = state;
+			this.zip = zip;
+			this.phone_no1 = phone_num1;
+			this.phone_no2 = phone_num2;
+			this.e_mail = e_mail;
+			this.web_mail = web_mail;
+		}
+		public String getFirst_name() {
+			return first_name;
+		}
+		public void setFirst_name(String first_name) {
+			this.first_name = first_name;
+		}
+		public String getLast_name() {
+			return last_name;
+		}
+		public void setLast_name(String last_name) {
+			this.last_name = last_name;
+		}
+		public String getCompany_name() {
+			return company_name;
+		}
+		public void setCompany_name(String company_name) {
+			this.company_name = company_name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getCountry() {
+			return country;
+		}
+		public void setCountry(String country) {
+			this.country = country;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getZip() {
+			return zip;
+		}
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+		public String getPhone_num1() {
+			return phone_num1;
+		}
+		public void setPhone_num1(String phone_num1) {
+			this.phone_num1 = phone_num1;
+		}
+		public String getPhone_num2() {
+			return phone_num2;
+		}
+		public void setPhone_num2(String phone_num2) {
+			this.phone_num2 = phone_num2;
+		}
+		public String getE_mail() {
+			return e_mail;
+		}
+		public void setE_mail(String e_mail) {
+			this.e_mail = e_mail;
+		}
+		public String getWeb_mail() {
+			return web_mail;
+		}
+		public void setWeb_mail(String web_mail) {
+			this.web_mail = web_mail;
+	
+			// TODO Auto-generated method stub
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+
